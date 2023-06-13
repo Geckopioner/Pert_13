775ebf0f81242c11683b5132765087b03822f6ce
Geckopioner <127809174+Geckopioner@users.noreply.github.com>
2023-06-13 12:16:10 +0800

    blm selesai

---
+++ Pert_14/test.1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+</head>
+<body>
+   <p align: center><h5>Dojing.net</h5></p>
+    <div id="isikonten"> Ceritaku</div> 
+
+<script>
+$(function(){
+    $("#isikonten").css("background-color : #ff0000");
+    $()
+});
+</script>
+
+    
+</body>
+</html>
